066afbb9b920c8c780a7c9a22c2678b6db7f3a87
Raphael Jaouen <20221321@etud.univ-evry.fr>
2025-10-30 10:34:55 +0100

    ajout lancer2

--- index.js
+++ index.js
@@ -1,20 +1,37 @@
-export function analyseThrowsOfDice([d1, d2, d3, d4, d5]){
+export function analyseThrowsOfDice([throw1dice1, throw1dice2, throw1dice3, throw1dice4, throw1dice5],[throw2dice1, throw2dice2, throw2dice3, throw2dice4, throw2dice5]){
 
-    let resultat;
-    let amountOfEach = [0, 0, 0, 0, 0, 0];
+    let resultat1;
+    let resultat2;
+    let amountOfEachThrow1 = [0, 0, 0, 0, 0, 0];
+    let amountOfEachThrow2 = [0, 0, 0, 0, 0, 0];
 
-    amountOfEach[d1]++;
-    amountOfEach[d2]++;
-    amountOfEach[d3]++;
-    amountOfEach[d4]++;
-    amountOfEach[d5]++;
+    amountOfEachThrow1[throw1dice1]++;
+    amountOfEachThrow1[throw1dice2]++;
+    amountOfEachThrow1[throw1dice3]++;
+    amountOfEachThrow1[throw1dice4]++;
+    amountOfEachThrow1[throw1dice5]++;
+
+    amountOfEachThrow2[throw2dice1]++;
+    amountOfEachThrow2[throw2dice2]++;
+    amountOfEachThrow2[throw2dice3]++;
+    amountOfEachThrow2[throw2dice4]++;
+    amountOfEachThrow2[throw2dice5]++;
 
     for(let i = 0; i <= 5; i++){
-        if(amountOfEach[i] == 5){
-            resultat = 50
+        if(amountOfEachThrow1[i] == 5){
+            resultat1 = 50
         }
-        
+        if(amountOfEachThrow2[i] == 5){
+            resultat2 = 50
+        }
+
+        if(amountOfEachThrow1[i] == 4){
+            resultat1 = 40
+        }
+        if(amountOfEachThrow2[i] == 4){
+            resultat2 = 40
+        }
     }
 
-    return resultat;
+    return resultat1 + resultat2;
 }
--- index.test.js
+++ index.test.js
@@ -4,7 +4,11 @@ import { analyseThrowsOfDice } from "./index.js";
 describe("Analyse dice throws", () => {
  
     it("if all same number", () => {
-        expect(analyseThrowsOfDice([1, 1, 1, 1, 1])).toBe(50);
+        expect(analyseThrowsOfDice([1, 1, 1, 1, 1], [1, 1, 1, 1, 1])).toBe(100);
+    });
+
+    it("same number four times", () => {
+        expect(analyseThrowsOfDice([1, 1, 1, 1, 2], [1, 1, 1, 1, 2])).toBe(80);
     });
 
 });
